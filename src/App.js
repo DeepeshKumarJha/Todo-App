f71b4a72e84ad5497fe5ef16f43d487a77bdfbd4
deepesh <dkj@deepeshs-MacBook-Air.local>
2021-12-28 16:48:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div>
+      <div className="todo-input"></div>
+      <div className='todo-list'>
+        <div className='todo-list-options'></div>
+      </div>
     </div>
   );
 }
